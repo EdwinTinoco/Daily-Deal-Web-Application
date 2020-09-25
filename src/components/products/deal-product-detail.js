8e87336a37b172cc5fc8a17fe132cf653a43d872
EdwinTinoco <jaredlotic@gmail.com>
2020-09-25 15:56:39 -0600

    updated countdown in product deal, added check stock with interval every second to check the stock

--- src/components/products/deal-product-detail.js
+++ src/components/products/deal-product-detail.js
@@ -1,8 +1,9 @@
 import React from "react";
-import axios from "axios";
+import DateCountdown from 'react-date-countdown-timer';
 
 export default function DealProductDetail(props) {
    const {
+      deal_finished_date,
       picture_product,
       product_title,
       product_description,
@@ -29,6 +30,12 @@ export default function DealProductDetail(props) {
 
                <p className="left">LEFT</p>
             </div>
+         </div>
+
+         <div className="countdown-deal">
+            <p>Deal ends in...</p>
+            {/* <p>{deal_finished_date}</p> */}
+            <DateCountdown dateTo={'2020-09-26 08:30:10'} callback={() => alert('Hello')} />
          </div>
 
       </div>
