d344d20f8d429aa490d23a784ba965f071115a4c
EdwinTinoco <jaredlotic@gmail.com>
2020-10-02 20:46:06 -0600

    updated signup to hooks

--- src/components/auth/login-customer.js
+++ src/components/auth/login-customer.js
@@ -50,7 +50,7 @@ export default function LoginCustomer(props) {
          errors["email"] = "Please enter your email";
       }
 
-      if (email !== "undefined") {
+      if (typeof email !== "undefined") {
          var pattern = new RegExp(/^(("[\w-\s]+")|([\w-]+(?:\.[\w-]+)*)|("[\w-\s]+")([\w-]+(?:\.[\w-]+)*))(@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$)|(@\[?((25[0-5]\.|2[0-4][0-9]\.|1[0-9]{2}\.|[0-9]{1,2}\.))((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\.){2}(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\]?$)/i);
 
          if (!pattern.test(email)) {
