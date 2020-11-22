a7b871c82a5b584e2f274d81382981fe60ded812
edwintinoco <jaredlotic@gmail.com>
2020-11-21 20:15:37 -0700

    added full pagination functionallity in master dashboard

--- src/components/master/master-dashboard.js
+++ src/components/master/master-dashboard.js
@@ -4,7 +4,6 @@ import moment from 'moment';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { Bar, Pie, Doughnut, Line } from 'react-chartjs-2';
 import Pagination from "react-js-pagination";
-// require("bootstrap/less/bootstrap.less");
 
 import NavigationBar from "../navigation-bar/navigation-bar"
 import AllActiveDealsList from './all-active-deals-list'
@@ -43,20 +42,19 @@ export default function MasterDashboard(props) {
    const [showSpinner, setShowSpinner] = useState("none")
    const [showSpinner2, setShowSpinner2] = useState("none")
    const [activePage, setActivePage] = useState(1)
-   const [perPage, setPerPage] = useState(3)
-   const [pageRange, setPageRange] = useState(5)
-   const [offset, setOffset] = useState(0)
+   const [perPage] = useState(10)
+   const [pageRange] = useState(5)
    const [totalRecords, setTotalrecords] = useState(0)
+   const [resultsRecords, setResultsRecords] = useState(0)
 
    const handlePageChange = (pageNumber) => {
-      console.log(`active page is ${pageNumber}`);
       setActivePage(pageNumber);
-      setOffset(pageNumber * perPage)
 
-      getAllActiveDealsList()
+      var offset = (pageNumber - 1) * perPage
+      getAllActiveDealsList(offset)
     }
 
-   const getAllActiveDealsList = async () => {
+   const getAllActiveDealsList = async (offset) => {
       setShowSpinner2("block")
 
       await axios.post(`${devEnv}/api/ma/all-active-deals`,
@@ -71,9 +69,13 @@ export default function MasterDashboard(props) {
          setAllActiveDeals(
             response.data['all_deals']
          )
-
+         
          setTotalrecords(
             response.data['total_records']['@total_records']
+         )
+         
+         setResultsRecords(
+            response.data['all_deals'].length
          )
 
          setShowSpinner2("none")
@@ -176,7 +178,9 @@ export default function MasterDashboard(props) {
 
    useEffect(() => {
       getBaChartAllDealsTotalsSales()
-      getAllActiveDealsList()
+
+      var offset = 0;
+      getAllActiveDealsList(offset)
    }, [])
 
    return (
@@ -233,46 +237,46 @@ export default function MasterDashboard(props) {
                      <div className="title">
                         <h2>Deals List</h2>
                      </div>
-
                     
-                        {showSpinner2 === "none" ? 
-                           (
+                     {showSpinner2 === "none" ? 
+                        (
+                           <table id='active-deals-table'>
+                              <tbody>
+                                 <tr>{tableHeaderAllActiveDeals()}</tr>
+                                 {allAcitveDealsItems()}
+                              </tbody>     
+                           </table>     
+                        ):
+                        (
+                           <div>
                               <table id='active-deals-table'>
                                  <tbody>
                                     <tr>{tableHeaderAllActiveDeals()}</tr>
-                                    {allAcitveDealsItems()}
-                                 </tbody>     
-                              </table>     
-                           ):
-                           (
-                              <div>
-                                 <table id='active-deals-table'>
-                                    <tbody>
-                                       <tr>{tableHeaderAllActiveDeals()}</tr>
-                                    </tbody>
-                                 </table>    
+                                 </tbody>
+                              </table>    
 
-                                 <div className="spinner2" style={{ display: showSpinner2 }}>
-                                    <FontAwesomeIcon icon="spinner" spin /><p>Loading...</p>
-                                 </div> 
-                              </div>
-                           )
-                        }
+                              <div className="spinner2" style={{ display: showSpinner2 }}>
+                                 <FontAwesomeIcon icon="spinner" spin /><p>Loading...</p>
+                              </div> 
+                           </div>
+                        )
+                     }
                      
+                     <div className="results-pagination">
+                        <p>{`Results: ${resultsRecords} records`}</p>
 
-                     <Pagination
-                        prevPageText='prev'
-                        nextPageText='next'
-                        firstPageText='first'
-                        lastPageText='last'
-                        activePage={activePage}
-                        itemsCountPerPage={perPage}
-                        totalItemsCount={totalRecords}
-                        pageRangeDisplayed={pageRange}
-                        onChange={handlePageChange}
-                        itemClass="page-item"
-                        linkClass="page-link"
-                     />
+                        <Pagination
+                           prevPageText='<'
+                           nextPageText='>'
+                           firstPageText='<<'
+                           lastPageText='>>'
+                           activePage={activePage}
+                           itemsCountPerPage={perPage}
+                           totalItemsCount={totalRecords}
+                           pageRangeDisplayed={pageRange}
+                           onChange={handlePageChange}
+                        />
+                     </div>
                   </div>
                </div>
             )
